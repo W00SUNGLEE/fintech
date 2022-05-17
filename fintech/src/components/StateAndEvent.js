9cbb13c531872d2dc2e1dafaf1ee8150d080831b
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-05-17 16:04:07 +0900

    react_state

--- fintech/src/components/StateAndEvent.js
+++ fintech/src/components/StateAndEvent.js
@@ -1,19 +1,19 @@
 //rafce : react func export
 
-import React from 'react'
+import React, { useState } from 'react'
 
 export const StateAndEvent = () => {
 
-    let userText = "사용자 입력 내용"
-
+    //let userText = "사용자 입력 내용"
+    const [userText, setUserText] = useState("초기 값 입니다.")
+            //이름,  변경할함수
     const handleClick = () => {
         alert("test");
     }
 
     const handleChange = (event) => {
         const {value} = event.target;
-        userText = value;
-        console.log(userText);
+        setUserText(value);
     }
 
   return (
