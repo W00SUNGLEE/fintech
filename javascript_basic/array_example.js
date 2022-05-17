16822ff667e2c3540ae08e0cc7c0d41629422619
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-05-17 13:15:50 +0900

    array_example

--- javascript_basic/array_example.js
+++ javascript_basic/array_example.js
@@ -9,3 +9,18 @@ console.log(array)
 //in java
 //private String [] StringArray = new String[6];
 //private ArrayList<String> arrayListString = new ArrayList<String>
+
+//가장 기본적인 반복문의 표현법
+for(let i = 0; i < array.length; i++){
+    console.log(array[i])
+}
+
+//for ... of의 사용법
+for(element of array){
+    console.log(element);
+}
+
+//array.map(function ()=>{})
+array.map((element)=> {
+    console.log(element)
+})
