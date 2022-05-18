d1e49c6d067fe7edb1412db7026917b1dc2621e9
W00SUNGLEE <vhxmfltm135@gmail.com>
2022-05-18 17:31:07 +0900

    AuthPage 임시

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ArrayMap from './components/ArrayMap'
 import AxiosTest from './page/AxiosTest';
 import NewsApiPage from './page/NewsApiPage';
+import AuthPage from './page/AuthPage';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/b" element={<ArrayMap/>}></Route>
         <Route path="/axiosTest" element={<AxiosTest/>}></Route>
         <Route path='/newsApiPage' element={<NewsApiPage/>}></Route>
+        <Route path='' element={<AuthPage/>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ fintech/src/page/AuthPage.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import AppBar from '../components/common/AppBar'
+import styled from 'styled-components'
+
+const AuthButton = styled.button`
+    border: 1px dotted #000;
+    background-color: #282c34;
+    color: white;
+    text-align: center;
+`
+
+const AuthPage = () => {
+  
+    const handleAuthButtonClick = () => {
+        //새창으로 인증사이트 오픈
+        const authPageUrl = "https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id=b27b0251-26c4-4051-9559-fed2970864fe&redirect_uri=http://localhost:3000/authResult&scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0"
+        window.open(authPageUrl)
+    }
+  
+    return (
+    <>
+        <AppBar title={"사용자 인증"}></AppBar>
+        <AuthButton onClick={handleAuthButtonClick}>사용자 인증</AuthButton>
+    </>
+  )
+}
+
+export default AuthPage
